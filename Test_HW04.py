0ff8ecba8f764773941435feeb5b2a3f1fb1b42a
Mako227 <82687614+Mako227@users.noreply.github.com>
2021-10-09 15:14:34 -0400

    Changed to reflect new source file

--- Test_HW04.py
+++ Test_HW04.py
@@ -1,32 +1,26 @@
 """
    File Name:    Test_HW04.py
    Author:       Alan Clark
-   Date:         2 Oct 2021
-   Description:  Test get_github_commits function
+   Date:         9 Oct 2021
+   Description:  Modified for HW05a -- test get_github_commits function while mocking all service requests
 """
 
 import unittest
+import unittest.mock
 
-from HW04_Alan_Clark import get_github_commits
+from HW04_Alan_Clark_mod import get_github_commits
 
 # This code implements the unit test functionality
 
 class Test_HW04(unittest.TestCase):
     """ Class for testing the one and only function in HW04 """
 
-    # Test 0
-    def testRunTheFunction(self):
-        """ No actual test -- just run the function twice for demonstration purposes """
-        print('No actual test here, just two demonstration runs')
-        get_github_commits('Mako227')
-        get_github_commits('richkempinski')
-        
     # Test 1
-#    def testMako227(self): 
-#        self.assertEqual(get_github_commits('Mako227'), """Repo: SSW567_HW02, Number of commits: 13\n
-#Repo: SSW567_HW04, Number of commits: 2\n
-#Repo: Student-Repository, Number of commits: 10""")
-
+    @mock.patch('requests.get')
+        def testMako227(self, mockedReq):
+            mockedReq.return_value = MockResponse('[{"sha":1},{"sha":2},{"sha":3},{"sha":4},{"sha":5},{"sha":6}]')
+            commits = get_github_commits('Mako227', 'SSW567_HW02')
+            self.assertEqual(commits, 13)
 
 # This runs all the test case functions from the classes in this file.
 if __name__ == '__main__':
